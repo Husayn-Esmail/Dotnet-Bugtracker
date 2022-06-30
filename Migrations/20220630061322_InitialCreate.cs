166cad781ef436283cafcace7e1afde61bca311d
Husayn Esmail <31267171+Husayn-Esmail@users.noreply.github.com>
2022-06-30 00:13:52 -0600

    modified gitignore to omit db

---
+++ Migrations/20220630061322_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace bugtracker.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Issue",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Status = table.Column<string>(type: "TEXT", nullable: true),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Priority = table.Column<string>(type: "TEXT", nullable: true),
+                    IssueType = table.Column<string>(type: "TEXT", nullable: true),
+                    DateTimeCreated = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DateTimeModifed = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    LastModifiedBy = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Issue", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Issue");
+        }
+    }
+}
