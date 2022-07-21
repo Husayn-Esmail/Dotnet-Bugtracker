001411967ec5f9dd6e6d99841fbed464a32587b8
Husayn Esmail <31267171+Husayn-Esmail@users.noreply.github.com>
2022-07-21 17:15:05 -0600

    fixed the modal close button so that it doesn't submit everytime it's clicked

--- wwwroot/js/modal.js
+++ wwwroot/js/modal.js
@@ -2,12 +2,16 @@
 
 const openEls = document.querySelectorAll("[data-open]");
 const isVisible = "is-visible";
+
+// allows user to click plus button to open modal
 for (const el of openEls) {
     el.addEventListener("click", function() {
         const modalId = this.dataset.open;
         document.getElementById(modalId).classList.add(isVisible);
     });
 }
+
+// close button
 const closeEls = document.querySelectorAll("[data-close]");
 for (const el of closeEls) {
     el.addEventListener("click", function() {
@@ -15,12 +19,15 @@ for (const el of closeEls) {
     });
 }
 
+// click outside exits modal
 document.addEventListener("click", e => {
     if (e.target == document.querySelector(".modal.is-visible")) {
         document.querySelector(".modal.is-visible").classList.remove(isVisible);
     }
 });
 
+
+// allows user to press escape to get out of modal
 document.addEventListener("keyup", e=> {
     if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
         document.querySelector(".modal.is-visible").classList.remove(isVisible);
