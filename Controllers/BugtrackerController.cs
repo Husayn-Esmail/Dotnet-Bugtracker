fb966760dc161c1a0e9145858ab2e5f4483b2eed
Husayn Esmail <31267171+Husayn-Esmail@users.noreply.github.com>
2022-07-31 22:35:15 -0600

    Added functionality to the edit button, not styled yet

--- Controllers/BugtrackerController.cs
+++ Controllers/BugtrackerController.cs
@@ -46,5 +46,60 @@ namespace bugtracker.Controllers
             }
             return View(issue);
         }
+
+        // GET: Issue/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Issue == null)
+            {
+                return NotFound();
+            }
+
+            var issue = await _context.Issue.FindAsync(id);
+            if (issue == null)
+            {
+                return NotFound();
+            }
+            return View(issue);
+        }
+        
+        // POST: Issue/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Status,Title,Description,Priority,IssueType")] Issue issue)
+        {
+            if (id != issue.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(issue);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!IssueExists(issue.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(issue);
+        }
+
+
+        private bool IssueExists(int id)
+        {
+            return (_context.Issue?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
     }
 }
