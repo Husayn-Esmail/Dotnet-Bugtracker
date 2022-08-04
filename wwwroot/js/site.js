601e9ab074467b2c8e5678f6cb936d31147cf99c
Husayn Esmail <31267171+Husayn-Esmail@users.noreply.github.com>
2022-08-04 09:05:10 -1000

    wrote JS to add top and bottom classes to respective issue items

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const issues = document.querySelectorAll('.issue');
+const finalItem = issues[issues.length - 1];
+const firstItem = issues[0];
+
+firstItem.querySelector('a').classList.add('top')
+finalItem.querySelector('a').classList.add('bottom');
