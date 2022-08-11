54dd34c0321567efa9c62e312a3b1da2e0401319
Husayn-Esmail <husayn@esmail.ca>
2022-08-10 22:57:50 -0600

    wrote js to add style based on priority

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -10,3 +10,23 @@ const firstItem = issues[0];
 
 firstItem.querySelector('a').classList.add('top')
 finalItem.querySelector('a').classList.add('bottom');
+
+
+// dynamically adds correct class to priority
+const priority = document.querySelectorAll('.priority');
+for (var i = 0; i < priority.length; i++) {
+    switch (priority[i].innerHTML) {
+        case "high":
+            priority[i].classList.add("high");
+            break;
+        case "medium":
+            priority[i].classList.add("medium");
+            break;
+        case "low":
+            priority[i].classList.add("low");
+            break;
+        default:
+            priority[i].classList.add("default");
+            break;
+    }
+}
